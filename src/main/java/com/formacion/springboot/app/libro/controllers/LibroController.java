1e8d23e88f674d372c4e0c35611a757f5eadb264
Elisa <egomezvivas@fusap.com.ar>
2020-07-08 13:30:26 -0300

    CRUD Creacion de los metodos para crear, actualizar y eliminar libros

--- src/main/java/com/formacion/springboot/app/libro/controllers/LibroController.java
+++ src/main/java/com/formacion/springboot/app/libro/controllers/LibroController.java
@@ -3,8 +3,14 @@ package com.formacion.springboot.app.libro.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacion.springboot.app.libro.models.entity.Libro;
@@ -27,4 +33,26 @@ public class LibroController {
 		return libroService.findById(id);
 		
 	}
+	
+	@PostMapping("/libros")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Libro crearLibro(@RequestBody Libro libro) {
+		return libroService.save(libro);
+	}
+	
+	@PutMapping("/libros/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+		public Libro actualizarLibro(@RequestBody Libro libro, @PathVariable Long id) {
+		Libro libroDB = libroService.findById(id);	
+		libroDB.setEditorial(libro.getEditorial());
+		libroDB.setValor(libro.getValor());
+		return libroService.save(libroDB);
+	}
+	
+	@DeleteMapping("/libros/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminarLibro(@PathVariable Long id) {
+		libroService.deleteById(id);
+	}
+	
 }
--- src/main/java/com/formacion/springboot/app/libro/models/service/LibroServiceImpl.java
+++ src/main/java/com/formacion/springboot/app/libro/models/service/LibroServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.formacion.springboot.app.libro.models.dao.LibroDao;
 import com.formacion.springboot.app.libro.models.entity.Libro;
@@ -16,6 +17,7 @@ public class LibroServiceImpl implements LibroService{
 	private LibroDao libroDao;
 	
 	@Override
+	@Transactional(readOnly = true )
 	public List<Libro> findAll() {
 		return libroDao.findAll();
 	}
@@ -23,6 +25,18 @@ public class LibroServiceImpl implements LibroService{
 	@Override
 	public Libro findById(Long id) {
 		return libroDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Libro save(Libro libro) {
+		return libroDao.save(libro);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(Long id) {
+		libroDao.deleteById(id);
 	}
 
 }

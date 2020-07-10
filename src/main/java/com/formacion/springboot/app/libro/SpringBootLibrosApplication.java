662e3fea67bc3612d6f0d80a6a7fc25476a39939
Elisa <egomezvivas@fusap.com.ar>
2020-07-10 16:06:10 -0300

    Common Para la organizacion de entidades comunes entre microservicios

--- src/main/java/com/formacion/springboot/app/libro/SpringBootLibrosApplication.java
+++ src/main/java/com/formacion/springboot/app/libro/SpringBootLibrosApplication.java
@@ -2,10 +2,12 @@ package com.formacion.springboot.app.libro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.formacion.springboot.app.common.models.entity"})
 public class SpringBootLibrosApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/formacion/springboot/app/libro/models/dao/LibroDao.java
+++ src/main/java/com/formacion/springboot/app/libro/models/dao/LibroDao.java
@@ -2,7 +2,7 @@ package com.formacion.springboot.app.libro.models.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.formacion.springboot.app.libro.models.entity.Libro;
+import com.formacion.springboot.app.common.models.entity.Libro;
 
 
 public interface LibroDao  extends JpaRepository<Libro, Long>{
